618f6f46855cc65dc462fc8dd6ccdd55348aa393
Moises Pinzon <moisespinzon97@hotmail.com>
2024-12-11 01:32:10 +0100

    ProyectoSprintBoot-Java

---
+++ FullStack-Java/src/main/java/com/pruebaTecnica/FullStack_Java/FullStackJavaApplication.java
@@ -0,0 +1,14 @@
+package com.pruebaTecnica.FullStack_Java;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackJavaApplication.class, args);
+	}
+
+}
